6ce5520651f0799de13181e0e4a7fa9d9d7e0313
Osama Yousuf <35128083+osama-usuf@users.noreply.github.com>
2020-01-21 22:32:21 +0500

    Jan '20 updated

---
+++ js/typeme.js
@@ -0,0 +1,9 @@
+var typed = $(".typed");
+
+$(() => {
+	typed.typed({
+	  strings: ["programming.", "designing.",  "teaching.", "playing the violin.", "studying."],
+	  typeSpeed: 140,
+	  loop: true,
+	});
+});
